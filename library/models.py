e6c6b1355d5509621098fd71b2dc2fa84e717f90
Rajesh Kumar Yadav <68731452+initial1ze@users.noreply.github.com>
2025-06-20 12:12:49 +0530

    Update models.py

--- library/models.py
+++ library/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     publication_year = models.IntegerField()
     isbn = models.CharField(max_length=13, blank=True, null=True)
